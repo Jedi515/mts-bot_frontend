ef5a8c06aa8139701fccbbd56980f58492a97c15
Jedi515 <44167564+Jedi515@users.noreply.github.com>
2023-02-17 20:21:03 +1000

    Burger fix maybe

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,5 @@
-$('.menu-btn').on('click', function(e){
+$('.menu-nav').on('click', function(e){
     e.PreventDefault;
-    $(this).toggleClass('menu-btn_active');
-    $('.menu-nav').toggleClass('menu-nav_active');
+    $(this).toggleClass('menu-nav_active');
+    $('.menu-btn').toggleClass('menu-btn_active');
   });
