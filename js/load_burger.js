0755b2cb7c15ab5be457dd9c585999bad6d03472
Jedi515 <jedimailpost515@mail.ru>
2023-02-18 10:19:08 +1000

    Move trigger into load

--- js/load_burger.js
+++ js/load_burger.js
@@ -4,4 +4,9 @@ fetch("./burger.html")
   })
   .then(data => {
     document.querySelector("header").innerHTML = data;
+    $('.menu-btn').on('click', function(e){
+      e.PreventDefault;
+      $(this).toggleClass('menu-btn_active');
+      $('.menu-nav').toggleClass('menu-nav_active');
+    });
   });
